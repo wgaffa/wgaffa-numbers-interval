3652eb74b266c1e4c3595b0985077d811f0fa550
Patrik Maunus <subscription@skriptladan.se>
2019-08-27 16:47:52 +0200

    Add EndPoints

---
+++ Wgaffa.Numbers.Interval/EndPoint.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Wgaffa.Numbers
+{
+    public class EndPoint<T> : IComparable<T> where T : IComparable<T>
+    {
+        public static EndPoint<T> PositiveInfinity = new EndPointInfinity(1);
+        public static EndPoint<T> NegativeInfinity = new EndPointInfinity(-1);
+
+        public T Value { get; }
+
+        public EndPoint(T value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator EndPoint<T>(T value)
+        {
+            return new EndPoint<T>(value);
+        }
+
+        public virtual int CompareTo(T other)
+        {
+            return Value.CompareTo(other);
+        }
+
+        class EndPointInfinity : EndPoint<T>
+        {
+            private int _returnValue;
+
+            public EndPointInfinity(int compareReturn)
+                : base(default)
+            {
+                _returnValue = compareReturn;
+            }
+
+            public override int CompareTo(T other)
+            {
+                return _returnValue;
+            }
+        }
+    }
+}
---
+++ Wgaffa.Numbers.Tests/EndPointTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+namespace Wgaffa.Numbers.Tests
+{
+    [TestFixture]
+    public class EndPointTests
+    {
+        [Test]
+        public void Value_ShouldReturnCorrectValue()
+        {
+            var start = new EndPoint<int>(3);
+
+            Assert.That(start.Value, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void ImplicitConversion_ShouldSetCorrectValue()
+        {
+            EndPoint<int> start = 2;
+
+            Assert.That(start.Value, Is.EqualTo(2));
+        }
+
+        [TestCase(5, 10, ExpectedResult = -1)]
+        [TestCase(25, 7, ExpectedResult = 1)]
+        [TestCase(-5, 0, ExpectedResult = -1)]
+        [TestCase(-5, -5, ExpectedResult = 0)]
+        [TestCase(115, 75, ExpectedResult = 1)]
+        public int CompareTo_ShouldReturnCorrectOrder(int value, int compareTo)
+        {
+            var endPoint = new EndPoint<int>(value);
+
+            return endPoint.CompareTo(compareTo);
+        }
+
+        [TestCase(int.MaxValue)]
+        [TestCase(int.MinValue)]
+        [TestCase(0)]
+        public void CompareTo_ShouldReturnPositiveOrder_GivenPositiveInfinity(int compare)
+        {
+            var endPoint = EndPoint<int>.PositiveInfinity;
+
+            Assert.That(endPoint.CompareTo(compare), Is.EqualTo(1));
+        }
+
+        [TestCase(int.MaxValue)]
+        [TestCase(int.MinValue)]
+        [TestCase(0)]
+        public void CompareTo_ShouldReturnNegativeOrder_GivenNegativeInfinity(int compare)
+        {
+            var endPoint = EndPoint<int>.NegativeInfinity;
+
+            Assert.That(endPoint.CompareTo(compare), Is.EqualTo(-1));
+        }
+
+        [TestCase('f')]
+        [TestCase('a')]
+        [TestCase('z')]
+        public void Infinity_ShouldReturnPositive_GivenCharEndPoints(char x)
+        {
+            var endPoint = EndPoint<char>.PositiveInfinity;
+
+            Assert.That(endPoint.CompareTo(x), Is.EqualTo(1));
+        }
+    }
+}
