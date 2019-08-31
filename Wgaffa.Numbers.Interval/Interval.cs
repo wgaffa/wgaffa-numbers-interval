c967b71f83b1b29f76550a9cc4b5e5b6a682691c
Patrik Maunus <subscription@skriptladan.se>
2019-08-31 22:18:13 +0200

    Add Intersect to interval

--- Wgaffa.Numbers.Interval/Interval.cs
+++ Wgaffa.Numbers.Interval/Interval.cs
@@ -2,7 +2,7 @@
 
 namespace Wgaffa.Numbers
 {
-    public class Interval<T> where T : IComparable<T>
+    public class Interval<T> : IEquatable<Interval<T>> where T : IComparable<T>
     {
         public EndPoint<T> LowerBound { get; }
         public EndPoint<T> UpperBound { get; }
@@ -21,15 +21,50 @@ namespace Wgaffa.Numbers
 
         public Interval<T> Intersect(Interval<T> other)
         {
-            T max(T x, T y) => x.CompareTo(y) > 0 ? x : y;
-            T min(T x, T y) => x.CompareTo(y) < 0 ? x : y;
+            EndPoint<T> max(EndPoint<T> x, EndPoint<T> y) => x.CompareTo(y) > 0 ? x : y;
+            EndPoint<T> min(EndPoint<T> x, EndPoint<T> y) => x.CompareTo(y) < 0 ? x : y;
 
-            return new Interval<T>(max(LowerBound.Value, other.LowerBound.Value), min(UpperBound.Value, other.UpperBound.Value));
+            return new Interval<T>(max(LowerBound.Lower, other.LowerBound.Lower), min(UpperBound.Upper, other.UpperBound.Upper));
         }
 
         public override string ToString()
         {
             return $"{(LowerBound.Inclusive ? '[' : '(')}{LowerBound}, {UpperBound}{(UpperBound.Inclusive ? ']' : ')')}";
+        }
+
+        public bool Equals(Interval<T> other)
+        {
+            if (other == null)
+                return false;
+
+            return LowerBound.Equals(other.LowerBound) &&
+                UpperBound.Equals(other.UpperBound);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            return Equals(obj as Interval<T>);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = 17;
+                hashCode = hashCode * 223 + LowerBound.GetHashCode();
+                hashCode = hashCode * 223 + UpperBound.GetHashCode();
+
+                return hashCode;
+            }
         }
     }
 }
--- Wgaffa.Numbers.Tests/IntersectTests.cs
+++ Wgaffa.Numbers.Tests/IntersectTests.cs
@@ -10,6 +10,10 @@ namespace Wgaffa.Numbers.Tests
             new object[] { new Interval<int>(5, 10), new Interval<int>(2, 7), new Interval<int>(5, 7) },
             new object[] { new Interval<int>(1, 10), new Interval<int>(2, 7), new Interval<int>(2, 7) },
             new object[] { new Interval<int>(5, 5), new Interval<int>(2, 7), new Interval<int>(5, 5) },
+            new object[] { new Interval<int>(EndPoint<int>.NegativeInfinity, 5), new Interval<int>(2, 7), new Interval<int>(2, 5) },
+            new object[] { new Interval<int>(EndPoint<int>.Infinity, 5), new Interval<int>(2, 7), new Interval<int>(2, 5) },
+            new object[] { new Interval<int>(5, EndPoint<int>.PositiveInfinity), new Interval<int>(2, 7), new Interval<int>(5, 7) },
+            new object[] { new Interval<int>(5, EndPoint<int>.PositiveInfinity), new Interval<int>(2, EndPoint<int>.Infinity), new Interval<int>(5, EndPoint<int>.PositiveInfinity) },
         };
 
         [TestCaseSource(nameof(IntersectIntervalsSource))]
