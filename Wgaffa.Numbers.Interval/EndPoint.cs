af200eb0c0e8be0a2a0a6bd86ffdbaa15fa8c8a9
Patrik Maunus <subscription@skriptladan.se>
2019-08-31 08:57:20 +0200

    Add comparisons between two endpoints
    - Comparing two endpoints are now defined
    - Positive infinity is always higher than anything else except another
    Positive Infinity
    - Negative is always lower than anything else except itself
    - Infinity is treated as a positive infinity

--- Wgaffa.Numbers.Interval/EndPoint.cs
+++ Wgaffa.Numbers.Interval/EndPoint.cs
@@ -2,7 +2,7 @@
 
 namespace Wgaffa.Numbers
 {
-    public class EndPoint<T> : IComparable<T>, IComparable<EndPoint<T>> where T : IComparable<T>
+    public class EndPoint<T> : IComparable<T>, IComparable<EndPoint<T>>, IEquatable<EndPoint<T>> where T : IComparable<T>
     {
         public static EndPoint<T> PositiveInfinity = new EndPointPositiveInfinity();
         public static EndPoint<T> NegativeInfinity = new EndPointPositiveInfinity(-1);
@@ -61,6 +61,40 @@ namespace Wgaffa.Numbers
             return Value.ToString();
         }
 
+        public bool Equals(EndPoint<T> other)
+        {
+            if (other == null)
+                return false;
+
+            return CompareTo(other) == 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            return Equals(obj as EndPoint<T>);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = 17;
+                hashCode = hashCode * 223 + Value.GetHashCode();
+                hashCode = hashCode * 223 + Inclusive.GetHashCode();
+
+                return hashCode;
+            }
+        }
+
         class EndPointInfinity : EndPoint<T>
         {
             internal override EndPoint<T> Lower => NegativeInfinity;
@@ -73,7 +107,18 @@ namespace Wgaffa.Numbers
 
             public override int CompareTo(T other)
             {
-                return 0;
+                return 1;
+            }
+
+            public override int CompareTo(EndPoint<T> other)
+            {
+                if (other is EndPointPositiveInfinity inf)
+                    return inf.Sign < 0 ? 1 : 0;
+
+                if (other is EndPointInfinity)
+                    return 0;
+
+                return 1;
             }
 
             public override string ToString()
